b5c91024e0e7201caa020d2e4826618f72994c6e
SriramKaluva <kaluvasriram@gmail.com>
2021-03-09 16:06:05 +0530

    add like function with refresh

--- app/controllers/microposts_controller.rb
+++ app/controllers/microposts_controller.rb
@@ -1,11 +1,13 @@
 class MicropostsController < ApplicationController
   include ActionController::MimeResponds
   before_action :signed_in_user, only: [:create, :vote, :destroy]
-  before_action :correct_user,   only: [:vote, :destroy]
+  before_action :correct_user,   only: [:destroy]
   respond_to :js, :json, :html
   respond_to do |format|
-  format.js {}
-end
+    format.html
+    format.js {render layout: false}
+  end
+
 
   def index
   end
@@ -21,11 +23,12 @@ end
     end
   end
   def vote
-    if !current_user.liked? @micropost
-      @micropost.liked_by current_user
+    @micropost_all = Micropost.find(params[:id])
+    if !current_user.liked? @micropost_all
+      @micropost_all.liked_by current_user
       redirect_to root_url
-    elsif current_user.liked? @micropost
-      @micropost.unliked_by current_user
+    elsif current_user.liked? @micropost_all
+      @micropost_all.unliked_by current_user
       redirect_to root_url
     end
   end
@@ -39,6 +42,7 @@ end
     def micropost_params
       params.require(:micropost).permit(:caption, :image)
     end
+
     def correct_user
       @micropost = current_user.microposts.find_by(id: params[:id])
       redirect_to root_url if @micropost.nil?
