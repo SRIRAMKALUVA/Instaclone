629ec4b72fb1d18950e57c4f74530040625ad691
SriramKaluva <kaluvasriram@gmail.com>
2021-03-10 16:14:27 +0530

    change default DP

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -2,7 +2,12 @@ module UsersHelper
   # Returns the Gravatar (http://gravatar.com/) for the given user.
   def gravatar_for(user)
     gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
-    gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}"
+    # if gravatar_id.blank?
+      gravatar_url = "https://www.gravatar.com/avatar/00000000000000000000000000000000?d=https://cdn1.vectorstock.com/i/thumb-large/82/55/anonymous-user-circle-icon-vector-18958255.jpg"
+    # else
+    #   gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}"
+    # end
+
     image_tag(gravatar_url, alt: user.name, class: "gravatar")
   end
 end
