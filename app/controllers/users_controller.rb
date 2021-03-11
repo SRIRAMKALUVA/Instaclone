6f87ddea22b4f25bec9e5812c8f936185641dacf
SriramKaluva <kaluvasriram@gmail.com>
2021-03-11 18:34:39 +0530

    temporary removal of welcome mail

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -24,7 +24,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      UserMailer.with(user: @user).welcome_email.deliver_now
+      # UserMailer.with(user: @user).welcome_email.deliver_now
       sign_in @user
       flash[:success] = "Welcome to the Instaclone!"
       redirect_to @user
